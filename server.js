4b65990bafda6cb515005ca76ca8987f5d03e6ea
Richard.C <r.conde@live.com>
2019-10-25 12:54:05 -0400

    modify README, created server.js, ran npm i -y

---
+++ server.js
@@ -0,0 +1,15 @@
+//File Created Oct 25, 2019
+// Authors: L.Dominguez & R.Conde
+
+
+
+
+
+
+
+
+
+//----------Server.js Application Floor----------
+//----Developer Notes-----------------
+// Oct 25, R.Conde: 1. Run npm i -y on server.js file (created {} package-lock.json)
+                 // 2.
