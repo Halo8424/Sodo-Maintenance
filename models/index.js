23803633e3989f056647f7fc09c80abe9b02c2a7
Richard.C <r.conde@live.com>
2019-10-29 11:42:51 -0400

    upper case T in ticket in index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
 //--Exporting an object containing all of our models
     module.exports = {
-        ticket: require("./ticket"),
-        //Comment: require("./comment")
+        Ticket: require("./ticket"),
     }; 
