f54251b909461ff0371f470971d525a7ede4427b
Richard.C <r.conde@live.com>
2019-10-30 20:59:39 -0400

    save point

---
+++ controllers/models/ticket.js
@@ -0,0 +1,14 @@
+//--Dependencies
+    const mongoose = require("mongoose");
+    const Schema = mongoose.Schema;
+
+    const ticketSchema = new Schema({
+        title: { type: String, required: true},
+        manager: { type: String, required: true},
+        note: String,
+        date: { type: Date, default: Date.now}
+    });
+
+    const Ticket = mongoose.model("Ticket", ticketSchema);
+
+    module.exports = Ticket; 
