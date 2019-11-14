38bb989a647ce2543a1231e5f3d424224c999b58
Richard.C <r.conde@live.com>
2019-11-13 21:26:30 -0500

    styled Nav

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logoutUser } from '../../actions/authActions';
+import './style.css';
 
 class Navbar extends Component {
 
@@ -20,23 +21,23 @@ class Navbar extends Component {
             <div className="container">
 
                 <nav className="z-depth-0">
-                    <div className="nav-wrapper white" style={{ display: "flex", justifyContent: "space-between" }}>
-                        <Link to="/login" className="col s5 black-text" style={{ fontFamily: "monospace" }}>
+                    <div className="nav-wrapper" style={{ display: "flex", justifyContent: "space-between" }}>
+                        <Link to="/login" className="col s5 white-text nav-link" style={{ fontFamily: "monospace" }}>
                             Open Tickets
                             </Link>
 
                         {this.props.auth.isAuthenticated ?
 
                             <div style={{color: "gray"}}>
-                                <b style={{paddingRight: "7px", color: "gray"}}>HI, </b> {user.name} 
-                                <button className="waves-effect waves-teal btn-flat" onClick={this.onLogoutClick}>
-                                    Logout
+                                <b style={{paddingRight: "7px", color: "white", textShadow: "1px 1px 2px black", fontSize: "15px"}}>Welcome, </b> <b><span className="user-name">{user.name} </span></b>
+                                <button className="waves-effect waves-teal btn-flat" style={{ color: "White", textShadow: "1px 1px 2px black", fontSize: "15px"}} onClick={this.onLogoutClick}>
+                                   <b>Logout</b> 
                                 </button>
 
                             </div> :
                             <div>
-                                <Link to="/register" className="col s5 black-text" style={{ fontFamily: "monospace", paddingRight: "7px" }}>Register</Link>
-                                <Link to="/login" className="col s5 black-text" style={{ fontFamily: "monospace" }}>Login</Link>
+                                <Link to="/register" className="col s5 white-text nav-link-2" style={{ fontFamily: "monospace", paddingRight: "7px" }}>Register</Link>
+                                <Link to="/login" className="col s5 white-text nav-link-2" style={{ fontFamily: "monospace" }}>Login</Link>
                             </div>
                         }
                     </div>
