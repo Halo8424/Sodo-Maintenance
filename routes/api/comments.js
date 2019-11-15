d5f17a50aba2d2f381105dd0530cfca44da43fbe
Luis Dominguez <reynaldodominguez@gmail.com>
2019-11-15 10:41:31 -0500

    Code Cleaned

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -1,17 +1,13 @@
 const router = require("express").Router();
 const commentsController = require("../../controllers/commentsControllers");
 
-// Matches with "/api/tickets"
+
 router.route("/")
   .get(commentsController.findAll)
   .post(commentsController.create);
 
-// Matches with "/api/comments/:id"
 router
   .route("/:id")
-//   .get(commentsController.findById)
-//   .put(commentsController.update)
-//   .delete(commentsController.remove)
   .post(commentsController.create);
   
  
